4ef56d17757a92dbfc7a96f3683f5b4267092e4d
Pynsuklang <pynmaw@gmail.com>
2022-08-02 12:19:33 +0530

    commit for db configuration

---
+++ picapi/app/Http/Controllers/PicDBController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PicDB;
+use Illuminate\Http\Request;
+
+class PicDBController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PicDB  $picDB
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PicDB $picDB)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PicDB  $picDB
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PicDB $picDB)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PicDB  $picDB
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PicDB $picDB)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PicDB  $picDB
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PicDB $picDB)
+    {
+        //
+    }
+}
